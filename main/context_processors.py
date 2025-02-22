3cf7d8c980843d8d9fb9eb4cad628246996d8d16
AnastasiiaS13 <villasAVODES@gmail.com>
2025-02-22 10:29:44 +0100

    result anastasiia edit static root

--- main/context_processors.py
+++ main/context_processors.py
@@ -3,16 +3,30 @@ from .models import ContactInfo
 def contact_info(request):
     data=ContactInfo.objects.first()
 
-    return {
-        'location' : data.location,
-        'number' : data.number,
-        'email' : data.email,
-        'text' : data.text,
-        'opening_hours' : data.opening_hours,
+    if data:
+        return {
+            'location' : data.location,
+            'number' : data.number,
+            'email' : data.email,
+            'text' : data.text,
+            'opening_hours' : data.opening_hours,
 
-        'facebook_link' : data.facebook_link,
-        'twitter_link' : data.twitter_link,
-        'linkedin_link' : data.linkedin_link,
-        'instagram_link' : data.instagram_link,
-        'pinterest_link' : data.pinterest_link,
+            'facebook_link' : data.facebook_link,
+            'twitter_link' : data.twitter_link,
+            'linkedin_link' : data.linkedin_link,
+            'instagram_link' : data.instagram_link,
+            'pinterest_link' : data.pinterest_link,
+        }
+    return {
+        'location': '',
+        'number': '',
+        'email': '',
+        'text': '',
+        'opening_hours': '',
+
+        'facebook_link': '',
+        'twitter_link': '',
+        'linkedin_link': '',
+        'instagram_link': '',
+        'pinterest_link': '',
     }
